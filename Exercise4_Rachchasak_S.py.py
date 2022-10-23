b56d6116637ecdfc0a4fd97e0cfb0457674592a7
kong3161 <socialism.voice@gmail.com>
2022-10-23 14:20:58 +0700

    Exercise 4

---
+++ Exercise4_Rachchasak_S.py.py
@@ -0,0 +1,9 @@
+FoundationEnglish = 99.91
+GeneralBusiness = 99.92
+IntroductiontoComputerSystems = 99.93
+ComputerProgramming = 99.94
+print('--- Your Score ---')
+print("Foundation English : ", FoundationEnglish)
+print('General Business : ',GeneralBusiness)
+print('Introduction to Computer Systems : ',IntroductiontoComputerSystems)
+print('Computer Programming : ',ComputerProgramming)
