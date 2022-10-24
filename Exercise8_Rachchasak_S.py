e0b698dfcbdcbfe9617589a2d9ca6e4fa585d547
kong3161 <socialism.voice@gmail.com>
2022-10-24 11:05:31 +0700

    Exercise8

---
+++ Exercise8_Rachchasak_S.py
@@ -0,0 +1,26 @@
+Usernamelogin=input('User name : ')
+Passwordlogin=input('Password : ')
+if Usernamelogin=='admin' and Passwordlogin=='123456789':
+    print('successful login')
+    print('---Select a product list---')
+    print('1.Book 1 : 120 THB')
+    print('2.Book 2 : 150 THB')
+    print('3.Book 3 : 170 THB')
+    print('4.Book 4 : 200 THB')
+    Selectlist=int(input('Enter the selected item : '))
+    if Selectlist==1 or Selectlist==2 or Selectlist==3 or Selectlist==4:
+        Productneeded=int(input('Number of products needed : '))
+        if Selectlist == 1:
+            totalprice=120*Productneeded
+            print('Total price =',totalprice,'THB')
+        elif Selectlist == 2:
+            totalprice=150*Productneeded
+            print('Total price =',totalprice,'THB')
+        elif Selectlist == 3:
+            totalprice=170*Productneeded
+            print('Total price =',totalprice,'THB')
+        elif Selectlist == 4:
+            totalprice=200*Productneeded
+            print('Total price =',totalprice,'THB')
+else:
+    print('---Login failed---')
